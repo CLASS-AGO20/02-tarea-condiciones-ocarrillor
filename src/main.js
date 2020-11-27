2b96ceb8e2ae1beae943f080447fb62cf925863e
ocarrillor <ocarrillo7@ucol.mx>
2020-11-27 14:43:48 -0600

    Implemented function obtenerCalificacion

--- src/main.js
+++ src/main.js
@@ -35,6 +35,20 @@ export default class App {
                 return -1;
         }
     }
+
+    obtenerCalificacion(calificacionNumerica){
+        if((calificacionNumerica >= 0) && (calificacionNumerica <= 5)){
+            return "c";
+        }else if((calificacionNumerica == 6) || (calificacionNumerica == 7)){
+            return "b";
+        }else if((calificacionNumerica == 8) || (calificacionNumerica == 9)){
+            return "a";
+        }else if(calificacionNumerica == 10){
+            return "a+"
+        }else{
+            return "ERROR";
+        }
+    }
 }
 
 let app = new App();
@@ -50,3 +64,8 @@ console.log(app.esMultiplo(30,7));
 //Demostracion de la funcion esMultiplo.
 console.log(app.calcularSalario(1,5));
 console.log(app.calcularSalario(5,6));
+
+//Demostracion de la funcion obtenerCalificacion.
+console.log(app.obtenerCalificacion(5));
+console.log(app.obtenerCalificacion(10));
+console.log(app.obtenerCalificacion(-1));
