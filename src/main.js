84d3d828a07eb6a4ed961556952cfcae078d368b
ocarrillor <ocarrillo7@ucol.mx>
2020-11-27 14:09:49 -0600

    Implemented function esMultiplo

--- src/main.js
+++ src/main.js
@@ -8,10 +8,21 @@ export default class App {
             return false;
         }
     }   
+
+    esMultiplo(numero1, numero2){
+        if(numero1 % numero2 == 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
 
 let app = new App();
-
+//Demostracion de la funcion esPar.
 console.log(app.esPar(0));
 console.log(app.esPar(5));
 console.log(app.esPar(6));
+//Demostracion de la funcion esMultiplo.
+console.log(app.esMultiplo(20,4));
+console.log(app.esMultiplo(30,7));
