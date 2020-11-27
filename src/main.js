8923980254a1d20ccb1ea85460a8dce6cd8628e1
ocarrillor <ocarrillo7@ucol.mx>
2020-11-27 14:01:59 -0600

    Implemented function esPar

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
 export default class App {
- 
+    esPar(numero){
+        if(numero ==  0){
+            return false;
+        }else if(numero % 2 == 0){
+            return true;
+        }else{
+            return false;
+        }
+    }   
 }
 
 let app = new App();
+
+console.log(app.esPar(0));
+console.log(app.esPar(5));
+console.log(app.esPar(6));
